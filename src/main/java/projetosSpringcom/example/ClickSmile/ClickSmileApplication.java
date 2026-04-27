b4f392af7854cce1f34db4a69c6dc7dd02656834
p3drobitencourt <pedro.b1tencourt2801@gmail.com>
2026-04-27 16:35:56 -0300

    chore: initial commit and project structure

---
+++ src/main/java/projetosSpringcom/example/ClickSmile/ClickSmileApplication.java
@@ -0,0 +1,13 @@
+package projetosSpringcom.example.ClickSmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickSmileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickSmileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projetosSpringcom/example/ClickSmile/ClickSmileApplicationTests.java
@@ -0,0 +1,13 @@
+package projetosSpringcom.example.ClickSmile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickSmileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
